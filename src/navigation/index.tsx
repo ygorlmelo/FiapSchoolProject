7425412fabc6d9d3c27eb0138a573fc9cd8f31bb
Ygor Melo <ygor.melo@uniodigital.com.br>
2025-03-26 22:02:23 -0300

    Configuração das tabs

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
-import ClassesScreen from '../screens/ClassesScreen/ClassesScreen';
+import AulaScreen from '../screens/AulaScreen/AulaScreen';
+import BoletimScreen from '../screens/BoletimScreen/BoletimScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const AppNavigator: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="Login"
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Classes" component={ClassesScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+// Tab navigator com Aulas e Boletim
+const AppTabs = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName = route.name === 'Aulas' ? 'calendar' : 'document-text';
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#00FFFF',
+      tabBarInactiveTintColor: 'gray',
+      headerShown: false,
+    })}
+  >
+    <Tab.Screen name="Aulas" component={AulaScreen} />
+    <Tab.Screen name="Boletim" component={BoletimScreen} />
+  </Tab.Navigator>
+);
+
+const AppNavigator: React.FC = () => (
+  <Stack.Navigator
+    screenOptions={{ headerShown: false }}
+    initialRouteName="Login"
+  >
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="MainTabs" component={AppTabs} />
+  </Stack.Navigator>
+);
 
 export default AppNavigator;
